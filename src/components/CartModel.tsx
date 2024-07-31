296fc9b7b66f2a94ad5c7f391e5ca3bb6f5dec26
AtalibAgAlmousleck <atalibletouareg@gmail.com>
2024-07-31 17:38:29 +0800

    end of the nav and started of slider

--- src/components/CartModel.tsx
+++ src/components/CartModel.tsx
@@ -1,9 +1,90 @@
-"use client"
+"use client";
+
+import Image from "next/image";
 
 const CartModel = () => {
+  const cartItems = true;
   return (
-    <div className="absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">Cart Model</div>
-  )
+    <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
+      {!cartItems ? (
+        <div className="">Ops! your cart is empty</div>
+      ) : (
+        <>
+        <h2 className="text-xl">Shopping Cart</h2>
+          <div className="flex flex-col gap-8">
+            {/* Items */}
+            <div className="flex gap-4">
+              <Image
+                src="https://images.pexels.com/photos/26997905/pexels-photo-26997905/free-photo-of-woman-running-in-golden-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt=""
+                width={75}
+                height={96}
+                className="object-cover rounded-md"
+              />
+              <div className="flex flex-col justify-between w-full">
+                {/* TOP */}
+                <div className="">
+                  {/* TITLE */}
+                  <div className="flex items-center justify-between gap-8">
+                    <h3 className="font-semibold">Product name</h3>
+                    <div className="p-1 bg-gray-50 rounded-sm">$45</div>
+                  </div>
+                  {/* DESCRIPTION */}
+                  <div className="text-sm text-gray-500">Item description</div>
+                </div>
+                {/* BOTTOM */}
+                <div className="flex justify-between text-sm">
+                  <span className="text-gray-500">Qty: 2</span>
+                  <span className="text-red-500">remove</span>
+                </div>
+              </div>
+            </div>
+            {/* Items */}
+            <div className="flex gap-4">
+              <Image
+                src="https://images.pexels.com/photos/26997905/pexels-photo-26997905/free-photo-of-woman-running-in-golden-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt=""
+                width={75}
+                height={96}
+                className="object-cover rounded-md"
+              />
+              <div className="flex flex-col justify-between w-full">
+                {/* TOP */}
+                <div className="">
+                  {/* TITLE */}
+                  <div className="flex items-center justify-between gap-8">
+                    <h3 className="font-semibold">Product name</h3>
+                    <div className="p-1 bg-gray-50 rounded-sm">$45</div>
+                  </div>
+                  {/* DESCRIPTION */}
+                  <div className="text-sm text-gray-500">Item description</div>
+                </div>
+                {/* BOTTOM */}
+                <div className="flex justify-between text-sm">
+                  <span className="text-gray-500">Qty: 2</span>
+                  <span className="text-red-500">remove</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Bottoms */}
+          <div className="">
+            <div className="flex items-center justify-between font-semibold">
+              <span className="">Subtotal</span>
+              <span className="">$49</span>
+            </div>
+            <p className="text-gray-500 text-sm mt-2 mb-4">
+              Lorem ipsum dolor sit amet consectetur.
+            </p>
+            <div className="flex justify-between text-sm">
+              <button className="rounded-md py-3 px-4 ring-1 ring-gray-500">View Cart</button>
+              <button className="rounded-md py-3 px-4 bg-black text-white">Checkout</button>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default CartModel;
